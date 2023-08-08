b44eae67025cdf82709419869c8ab23f0aa0ae58
Erik <esnetto@gmail.com>
2023-08-08 19:57:46 +0000

    random? no lo creo

--- image_composite.py
+++ image_composite.py
@@ -5,10 +5,12 @@ import numpy as np
 from PIL import Image
 from itertools import product
 import json
-import openai  # Import the OpenAI library
+# import openai  # Import the OpenAI library
 
-# Set up your OpenAI API key here
-openai.api_key = ""
+# # Set up your OpenAI API key here
+# openai.api_key = ""
+
+random.seed(31337)
 
 root_dir = "./layers"  # directorio donde están las capas
 frames_dir = "./buttplugs"  # directorio de salida
@@ -56,56 +58,55 @@ gradient_image_path = os.path.join(root_dir, "Gradient.png")
 gradient_image = Image.open(gradient_image_path).convert('RGBA')
 
 
-def generate_background_story(properties):
-    try:
-        # Create a prompt for GPT-3.5 based on the NFT properties
-        traits = ""
-        for attribute in properties["attributes"]:
-            # Convert to lowercase
-            trait_type = attribute["trait_type"].lower()
-            traits += f"{trait_type}: {attribute['value']}\n"
+# def generate_background_story(properties):
+#     try:
+#         # Create a prompt for GPT-3.5 based on the NFT properties
+#         traits = ""
+#         for attribute in properties["attributes"]:
+#             # Convert to lowercase
+#             trait_type = attribute["trait_type"].lower()
+#             traits += f"{trait_type}: {attribute['value']}\n"
 
-        # Use the GPT-3.5 API to generate the background story
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {
-                    "role": "system",
-                    "content": "Generate a background story with this NFT traits, it is an osciloscope themed robot who is part of WebtrES club."
-                },
-                {
-                    "role": "user",
-                    "content": traits
-                }
-            ],
-            temperature=0.8,
-            max_tokens=100,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0
-        )
+#         # Use the GPT-3.5 API to generate the background story
+#         response = openai.ChatCompletion.create(
+#             model="gpt-3.5-turbo",
+#             messages=[
+#                 {
+#                     "role": "system",
+#                     "content": "Generate a background story with this NFT traits, it is an osciloscope themed robot who is part of WebtrES club."
+#                 },
+#                 {
+#                     "role": "user",
+#                     "content": traits
+#                 }
+#             ],
+#             temperature=0.8,
+#             max_tokens=100,
+#             top_p=1,
+#             frequency_penalty=0,
+#             presence_penalty=0
+#         )
 
-        # Extract the generated text from the response
-        generated_text = response.choices[0].message.content
+#         # Extract the generated text from the response
+#         generated_text = response.choices[0].message.content
 
-        last_full_stop_index = generated_text.rfind('.')
-        if last_full_stop_index != -1:
-            generated_text = generated_text[:last_full_stop_index + 1]
+#         last_full_stop_index = generated_text.rfind('.')
+#         if last_full_stop_index != -1:
+#             generated_text = generated_text[:last_full_stop_index + 1]
 
-        return generated_text
+#         return generated_text
 
-    except Exception as e:
-        print("Error while generating background story:", e)
-        return ""  # Return an empty string in case of error
+#     except Exception as e:
+#         print("Error while generating background story:", e)
+#         return ""  # Return an empty string in case of error
 
 
 def generate_properties(combination, idx):
     # Define the properties for the GIF based on the combination of subgroups
     properties = {
-        "description": "Webtres community NFTs.",
-        "external_url": "https://huffplug.com",
+        "description": "",
         # The GIF filename will be based on the index
-        "image": f"[placeholder]{idx+1}.gif",
+        "image": f"[placeholder]/{idx+1}.gif",
         "attributes": []
     }
 
@@ -146,9 +147,9 @@ for idx, combination in enumerate(combinations):
     # Generate properties for the current combination
     properties = generate_properties(combination, idx)
 
-    # Generate the background story based on the properties
-    background_story = generate_background_story(properties)
-    properties["description"] = background_story
+    # # Generate the background story based on the properties
+    # background_story = generate_background_story(properties)
+    # properties["description"] = background_story
 
     # Save the properties to a JSON file
     with open(os.path.join(frames_dir, f'{idx+1}.json'), 'w') as json_file:
